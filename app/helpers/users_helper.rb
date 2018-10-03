19b97dfdb4d73e0b73c39a779ced574200af6907
James McCann <jamesredux@gmail.com>
2018-10-03 20:43:21 +0700

    added user views to 7.2

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+	# Returns the Gravatar for the given user. size options added as extra exercise 
+	# see chapter 7 listing 7.9 for original
+	def gravatar_for(user, size: 80)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
 end
