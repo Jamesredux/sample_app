b6ed5270b14b8f364a89faeaa6a167c2bfb39c92
James McCann <jamesredux@gmail.com>
2018-10-06 20:14:59 +0700

    tests for unsuccessful signup to 7.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
 
-  get 'users/new'
+  
 
 	root 'static_pages#home'
   
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/about', 	to: 'static_pages#about'
   get '/contact',	to: 'static_pages#contact'
   get '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :users
   
 end
