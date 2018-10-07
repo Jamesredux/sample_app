dcdc7e4e32ad62cb4d5cc23b736c34ffc6ffeec5
James McCann <jamesredux@gmail.com>
2018-10-07 21:20:52 +0700

    complete to 7.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params) 
   	if @user.save
-  		#successful save add where to go
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
   	else
  		 	render 'new'
  		end 		
