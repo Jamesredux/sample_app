cd790ee303ead609048cea67b3ee6b4010777584
James McCann <jamesredux@gmail.com>
2018-10-17 21:38:47 +0700

    to 10.3.2

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,16 +1,32 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:michael)
+  end
+
+
   test "layout links" do 
+
   	get root_path
   	assert_template 'static_pages/home'
   	assert_select "a[href=?]", root_path, count: 2
   	assert_select "a[href=?]", help_path
   	assert_select "a[href=?]", about_path
   	assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", users_path, count: 0
+
+
+    log_in_as(@user)
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", user_path(@user)
+
   	get contact_path
   	assert_select "title", full_title("Contact")
     get signup_path
